caef3c9e9b37dd33525173658184703844ad4773
amalthomson <namalthomson@gmail.com>
2024-06-26 15:29:45 +0530

    integrate aws-ssm

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ exports.handler = async (event) => {
     try {
       const messageBody = JSON.parse(record.body);
       const { firstname, lastname } = messageBody;
-      console.log(`Hello ${firstname} ${lastname}\nThank you for reaching out, your request is under process.\nRegards`);
+      const country = process.env.country || 'Unknown';
+      console.log(`Hello ${firstname} ${lastname}`);
+      console.log(`Thank you for reaching out, your request is under process.`);
+      console.log(`Please select a Country of choice from "${country}"`);
+      console.log(`Regards`);
     } catch (error) {
       console.error('Error processing message:', error);
       console.log('Original message body:', record.body);
